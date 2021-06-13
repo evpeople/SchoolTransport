cafb602721447f6c33911c4a2287c8da36235ebf
developer <739125772@qq.com>
2021-06-14 02:04:50 +0800

    真正实现拥挤度随时间变化

--- Campus_Navigation/app/src/main/java/com/CampusNavigation/Map/Map.java
+++ Campus_Navigation/app/src/main/java/com/CampusNavigation/Map/Map.java
@@ -180,6 +180,10 @@ public class Map {
 
     private int updateTableEntry(TableEntry[] tableEntries, TableEntry known) {
         Path[] temp = this.paths[known.getNumOfBuilding()];
+        for (int i=0;i<=temp.length;i++)
+        {
+            temp[i].setCrowdDegree(temp[i].getStart().type);
+        }
         double dv = Double.POSITIVE_INFINITY;
         int minRoute = -1;
 //更新完之后，当前表中最低的点
